1de43fcdffd5be9aa500061600b9dd8df80fc3fb
therealsid19 <siddyboy2003@gmail.com>
2024-08-20 02:17:44 -0400

    Added collisions

--- game/main.py
+++ game/main.py
@@ -65,7 +65,11 @@ def next_turn(snake, food):
 
         del snake.squares[-1]
 
-    window.after(SPEED, next_turn, snake, food)
+    if check_collision(snake):
+        game_over()
+    
+    else:
+        window.after(SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
     global direction
@@ -86,8 +90,19 @@ def change_direction(new_direction):
         if direction != 'left':
             direction = new_direction
 
-def check_collision():
-    pass
+def check_collision(snake):
+    x,y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+    
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+        
+    return False
 
 def game_over():
     pass
